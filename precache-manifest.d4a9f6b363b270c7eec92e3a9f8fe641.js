19c85ed24970519b44fe57d1965b80e77ba55b23
Lucky Rathore <luckyrathore03@gmail.com>
2020-04-30 03:44:25 +0530

    Updates

---
+++ precache-manifest.d4a9f6b363b270c7eec92e3a9f8fe641.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e3334229be0ded7118c177f38a38083",
+    "url": "/Lucky-Rathore/diagnosis-ui/index.html"
+  },
+  {
+    "revision": "f516b30b9487527bf52d",
+    "url": "/Lucky-Rathore/diagnosis-ui/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "481db2fc010d00fd8b4c",
+    "url": "/Lucky-Rathore/diagnosis-ui/static/js/2.418c2ed0.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/Lucky-Rathore/diagnosis-ui/static/js/2.418c2ed0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f516b30b9487527bf52d",
+    "url": "/Lucky-Rathore/diagnosis-ui/static/js/main.df20c0b9.chunk.js"
+  },
+  {
+    "revision": "7dac3087b9c0d2d7d558",
+    "url": "/Lucky-Rathore/diagnosis-ui/static/js/runtime-main.7e62aca4.js"
+  }
+]);
