5ed4675fa7819891cd2518a4e6cde2be36e878ad
Lucky Rathore <luckyrathore03@gmail.com>
2020-05-20 19:58:54 +0530

    Updates

--- precache-manifest.d39f1e5f9b3285e77e515e96a19437dd.js
+++ precache-manifest.d39f1e5f9b3285e77e515e96a19437dd.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b961ef5084a51907a681dc5140f245e6",
+    "revision": "6c785621e4d165ddf725c1f4d69e940a",
     "url": "/diagnosis-ui/index.html"
   },
   {
-    "revision": "5ad8d3892d8d6b76393b",
+    "revision": "694dc8c8f97a73ba0fc5",
     "url": "/diagnosis-ui/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "c17d13d5370ea50f8b5c",
-    "url": "/diagnosis-ui/static/js/2.75114df1.chunk.js"
+    "revision": "bf91aeee79c4cad13d78",
+    "url": "/diagnosis-ui/static/js/2.f37fb5d8.chunk.js"
   },
   {
     "revision": "eac4cf64bf573541fd6dba29c78393a0",
-    "url": "/diagnosis-ui/static/js/2.75114df1.chunk.js.LICENSE.txt"
+    "url": "/diagnosis-ui/static/js/2.f37fb5d8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5ad8d3892d8d6b76393b",
-    "url": "/diagnosis-ui/static/js/main.4a8cb021.chunk.js"
+    "revision": "694dc8c8f97a73ba0fc5",
+    "url": "/diagnosis-ui/static/js/main.e749fba6.chunk.js"
   },
   {
     "revision": "ba26bbfc94d62a589071",
